3a1c1f229e0b59b1f903d7d135a53fdb6f8a9b0c
OuyangSolomon <15083995837@163.com>
2020-02-28 21:11:41 +0800

    添加碰撞检测、计分、分数转图片显示、爆炸动画、开始菜单

--- js/lib.js
+++ js/lib.js
@@ -1,18 +1,66 @@
 // 画布宽、高
 var W = 1200;
 var H = 600;
+/*
+ * 游戏状态
+ * 0：游戏初始界面
+ * 1：游戏开始
+ * 2：游戏结束
+ */
+var gameState = 0;
+
 var canvas;
 var context;
+
+var bomb_img_width = 40;
+var bomb_img_height = 20;
+var submarine_img_width = 135;
+var submarine_img_height = 110;
+
+// 按钮
+var startImg = getImage("img/go_-sheet0.png");
+var startX = (W-300)/2;
+var startY = (H-160)*3/5;
+var helpImg = getImage("img/question_mark_-sheet0.png");
+var helpX = W-200;
+var helpY = H*5/7;
+var soundImg = getImage("img/button_sounds-sheet1.png");
+var soundX = 100;
+var soundY = H*5/7;
+var titleImg = getImage("img/logo_-sheet0.png");
+var logo = getImage("img/icon-256.png");
+var bg = getImage("img/tiledbackground.png");
+
+// 说明书是否显示
+var isHelpText = false;
+var helpTextImg = getImage("img/help-sheet0.png");
+
+// 爆炸动画数组
+var explosions = [];
 // 定时器
 var gameTimer = null;
 var bgBottom = new BgBottom();
 var submarine = new Submarine();
 var bombs = [];
+var score = 0;
+var historyScore = localStorage.getItem("HappySubmarine_ouyang_score");
+// 潜艇碰撞顶部或者底部可否再加载动画
+var upEx = false;
+var downEx = false;
+
+var scoreImg = getImage("img/score.png");
 // 获取image对象
 function getImage(obj){
 	var img = new Image();
 	img.src = obj;
 	return img;
+}
+// 碰撞检测
+function hit(x, y, minx, miny, maxx, maxy){
+	if(x >= minx && x <= maxx && y >= miny && y <= maxy){
+		return true;
+	}
+	return false;
 }
 
 // 海底背景
@@ -58,12 +106,12 @@ function Submarine(){
 	this.isUp = false;
 	// 潜艇帧数
 	this.frameNum = 6;
+	// 潜艇生命数
+	this.life = 3;
 	// 帧数开始位置
 	this.frameStart = 0;
 	// 第几个帧图
-	this.frameSet = 0;
-	// 潜艇生命数
-	this.life = 3;
+	this.frameSet = this.frameStart;
 	this.heart = getImage("img/heart-sheet0.png");
 	this.heartH = 38;
 	this.heartW = 41;
@@ -79,6 +127,17 @@ function Submarine(){
 		
 		// 循环播放帧图
 		// 得到帧图所在的行列值
+		// 根据生命值自动更新显示的帧图
+		if(this.life == 1){
+			this.frameNum = 6;
+			this.frameStart = 6;
+		}else if(this.life > 1){
+			this.frameNum = 6;
+			this.frameStart = 0;
+		}else{
+			this.frameNum = 1;
+			this.frameStart = 12;
+		}
 		var x = Math.floor(this.frameSet / 4);
 		var y = this.frameSet % 4;
 		if(this.frameSet < this.frameStart + this.frameNum){
@@ -90,15 +149,25 @@ function Submarine(){
 		}
 	}
 	this.drawSubmarine = function(){
-		context.drawImage(this.img, this.sx, this.sy, this.w, this.h, this.x, this.y, 135, 110);
+		context.drawImage(this.img, this.sx, this.sy, this.w, this.h, this.x, this.y, submarine_img_width, submarine_img_height);
 	}
 	this.move = function(){
-		if(this.isUp){
-			this.vy = -7;
-			this.y += this.vy;
-		}else{
-			this.y += this.vy;
-			this.vy += this.avy;
+		if(this.life > 0){
+			if(this.isUp){
+				this.vy = -7;
+				this.y += this.vy;
+			}else{
+				this.y += this.vy;
+				this.vy += this.avy;
+			}
+		}
+		else{
+			if(this.y+5 < H-(this.h-85)-40){
+				this.y += this.vy;
+				this.vy += this.avy;
+			}else{
+				this.y = H-(this.h-85)-35;
+			}
 		}
 	}
 }
@@ -108,18 +177,81 @@ function Bomb(){
 	this.w = 105;
 	this.h = 47;
 	this.x = W;
-	this.y = Math.random()*(H - this.h - 135);
+	this.y = Math.random()*(H - this.h - 50);
 	this.img = getImage("img/torpedo-sheet0.png");
 	this.move = function(){
 		this.x -= 5;
 	}
 	this.drawBomb = function(){
-		//以右上点为中心 向右翻转画布
 		context.translate(2*this.x + this.w, 0);
 	    context.scale(-1, 1);
 	    //画图
-	    context.drawImage(this.img, 0, 0, this.w, this.h, this.x, this.y, 55, 30);
+	    context.drawImage(this.img, 0, 0, this.w, this.h, this.x, this.y, bomb_img_width, bomb_img_height);
 	    //翻转回来
 	    context.setTransform(1, 0, 0, 1, 0, 0);
+	}
+}
+
+// 爆炸特效
+// 0 表示小；1 表示大
+function Explosion(ex, ey, size){
+	this.x = ex;
+	this.y = ey;
+	this.w = 210;
+	this.h = 210;
+	this.size = size;
+	this.imgs = [];
+	// 总帧数
+	this.frameNum = 9;
+	// 每张图的帧图数
+	this.frames = [4,4,1];
+	// 现在的帧图序号
+	this.frameNow = 2;
+	// 现在是第几张图
+	this.i = 0;
+	this.sx = 0;
+	this.sy = 0;
+	for(var i = 0; i < 3; i++){
+		this.imgs[i] = getImage("img/explosion-sheet" + i + ".png");
+	}
+	this.updateFrame = function(){
+		this.i = Math.floor(this.frameNow / 4);
+		// 帧图所在的行列
+		var xx = Math.floor(this.frameNow%4/2);
+		var yy = this.frameNow%4%2;
+		if(this.frameNow < this.frameNum){
+			this.sx = yy*this.w;
+			this.sy = xx*this.h;
+			this.frameNow += 1;
+		}
+	}
+	this.drawExplosion = function(){
+		if(this.frameNow < this.frameNum){
+			if(this.size){
+				context.drawImage(this.imgs[this.i],this.sx,this.sy,this.w,this.h,this.x,this.y,150,150);
+			}else{
+				context.drawImage(this.imgs[this.i],this.sx,this.sy,this.w,this.h,this.x,this.y,80,80);
+			}
+		}
+	}
+}
+// 数字转图片
+function paintNum(x, y, num){
+	var img = getImage("img/number.png");
+	var w = 90;
+	var h = 100;
+	var sx = [];
+	var str = String(num);
+	for(var i = 0; i < str.length; i++){
+		var item = Number(str[i]);
+		if(item > 0){
+			sx.push((item - 1)*w);
+		}
+		if(item == 0){
+			sx.push(9*w);
+		}
+	}
+	for(var i = 0; i < sx.length; i++){
+		context.drawImage(img, sx[i], 0, w, h, x + i*(w-62), y, 45, 50);
 	}
 }
--- js/main.js
+++ js/main.js
@@ -2,6 +2,13 @@
 window.onload = function(){
 	canvas = document.getElementById("canvas");
 	context = canvas.getContext("2d");
+	if(!historyScore && typeof(historyScore)!="undefine" && historyScore!=0){
+		window.localStorage.setItem("HappySubmarine_ouyang_score", 0);
+		historyScore = 0;
+	}
+
+	setInterval(createBomb, 2000);
+	setInterval(gameLogic, 50);
 }
 
 window.onmousedown = function(){
@@ -13,26 +20,135 @@ window.onmouseup = function(){
 
 // 生成炸弹
 function createBomb(){
-	var bomb = new Bomb();
-	bombs.push(bomb);
+	if(gameState == 1){
+		var bomb = new Bomb();
+		bombs.push(bomb);
+	}
 }
 
-setInterval(createBomb, 2000);
-
-setInterval(function(){
+function gameLogic(){
 	context.clearRect(0, 0, W, H);
 	
-	// 潜艇
-	submarine.move();
-	submarine.drawSubmarine();
-	for(var i = 0; i < bombs.length; i++){
-		bombs[i].move();
-		bombs[i].drawBomb();
+	if(gameState == 1){
+		// 炸弹
+		for(var i = 0; i < bombs.length; i++){
+			bombs[i].move();
+			bombs[i].drawBomb();
+		}
+		// 潜艇
+		submarine.move();
+		submarine.drawSubmarine();
+		submarine.updateFrame();
+		// 海底
+		bgBottom.updateFrame();
+		bgBottom.drawBg();
+		
+		// 鱼雷与潜艇的碰撞检测
+		bombAndSubmarine();	
+		displayExplosion();
+		// 绘制分数
+		context.drawImage(scoreImg, W*4/6, 30, 160, 40);
+		getScore();
+		submarineMove();
+		if(submarine.life < 0){
+			gameState = 2;
+		}
+	}else if(gameState == 0){
+		context.drawImage(titleImg, (W-450) / 2, (H-150) / 5, 450, 200);
+		context.drawImage(startImg, startX,startY);
+		context.drawImage(bg, 0, H-180);
+		context.drawImage(bg, 1024, H-180);
+		context.drawImage(logo, 100, H / 2-100, 128,128);
+		context.drawImage(soundImg, soundX, soundY, 100, 100);
+		context.drawImage(helpImg, helpX,helpY, 100, 100);
+		if(isHelpText){
+			context.beginPath();
+			context.rect(0,0,W,H);
+			context.fillStyle = "rgba(0,0,0,0.7)";
+			context.fill();
+			context.closePath();
+			context.drawImage(helpTextImg, (W-986/1.3)/2,(H-708/1.3)/2, 986/1.3, 708/1.3);
+		}
+		canvas.onclick = function(e){
+			var ev = e || window.event;
+			// 开始按钮点击事件
+			if(hit(ev.offsetX, ev.offsetY, startX, startY, startX+300,startY+160)){
+				gameState = 1;
+				canvas.onclick = null;
+			}
+			if(hit(ev.offsetX, ev.offsetY, helpX, helpY, helpX+100, helpY+100)){
+				isHelpText = true;
+			}
+			
+		}
 	}
-	
-	submarine.updateFrame();
-	// 海底
-	bgBottom.updateFrame();
-	bgBottom.drawBg();
-	
-}, 50);
+}
+
+// 鱼雷与潜艇的碰撞检测
+function bombAndSubmarine(){
+	for(var i = 0; i < bombs.length; i++){
+		var bomb = bombs[i];
+		// 鱼雷：bomb.x + 65, bomb.y, bomb.w-65, bomb.h-25
+		// 潜艇：submarine.x + 5, submarine.y + 5, submarine.w-100, submarine.h-85
+		if(hit(bomb.x+65, bomb.y,
+			submarine.x+5 - (bomb.w-65),
+			submarine.y+5 - (bomb.h-25),
+			submarine.x+5 + (submarine.w-100),
+			submarine.y+5 + (submarine.h-85))
+		){
+			// 添加爆炸
+			explosions.push(new Explosion(bomb.x+65, bomb.y, 0));
+			bombs.splice(i, 1);
+			submarine.life--;
+		}
+	}
+}
+
+// 播放爆炸动画
+function displayExplosion(){
+	if(explosions.length > 0){
+		for(var i = 0; i < explosions.length; i++){
+			explosions[i].updateFrame();
+			explosions[i].drawExplosion();
+			if(explosions[i].frameNow == explosions[i].frameNum){
+				explosions.splice(i,1);
+			}
+		}
+	}
+//	console.log(explosions);
+}
+
+// 潜艇移动边界
+function submarineMove(){
+	if(submarine.y+5 < -(submarine.h-85-40)){
+		submarine.life = -10;
+		submarine.frameSet = 12;
+		if(!upEx){
+			explosions.push(new Explosion(submarine.x, submarine.y, 1));
+			upEx = true;
+		}
+	}
+	if(submarine.y+5 > H-(submarine.h-85)-45){
+		submarine.life = -10;
+		submarine.frameSet = 12;
+		if(!downEx){
+			explosions.push(new Explosion(submarine.x, submarine.y, 1));
+			downEx = true;
+		}
+	}
+}
+
+// 得分
+function getScore(){
+	if(submarine.life > 0){
+		for(var i = 0; i < bombs.length; i++){
+			var bomb = bombs[i];
+			if(bomb.x + 65 < -(bomb.w-65)){
+				bombs.splice(i,1);
+				score++;
+			}
+		}
+	}
+	// 绘制分数
+	paintNum(W*2/3 + 170, 25, score);
+}
